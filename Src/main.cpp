5460db8c2901d5981788b8da1891ae4fa050a9e8
Nisanth Mathew James <36531686+nisanthmathew@users.noreply.github.com>
2022-04-03 13:21:19 +0200

    added resource file
    Now the icon is added through resource file

--- Src/main.cpp
+++ Src/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.setWindowIcon(QIcon("H:\\EagleEyeSoftware\\g963.png"));
+    w.setWindowIcon(QIcon(":/icons/mainwindowicon.svg"));
     w.showMaximized();
     return a.exec();
 }
