16dfa03554ab08107d4f1c9501773aedcb6d4998
Nisanth Mathew James <36531686+nisanthmathew@users.noreply.github.com>
2022-09-08 07:22:04 +0200

    using QBechmark instead of manual time calculation

--- Src/Tests/loggertest.cpp
+++ Src/Tests/loggertest.cpp
@@ -33,19 +33,12 @@ void LoggerTest::LoggerPerfomanceTest()
 {
     QString testLog {"LoggerTest::LoggerPerfomanceTest(): testing, testing, testing."};
     std::string stdtestLog {"LoggerTest::LoggerPerfomanceTest(): testing, testing, testing."};
-    for (int i = 0; i < 10 ; i++)
+    QBENCHMARK
     {
-        auto start = std::chrono::high_resolution_clock::now();
-        for (int j = 0; j < 100 ; j++)
-        {
-            EagleEye::Logger::CENTRAL_LOGGER().LogMessage(testLog.toStdString(), EagleEye::LOGLEVEL::EE_DEBUG);
-            EagleEye::Logger::CENTRAL_LOGGER().LogMessage("LoggerTest::LoggerPerfomanceTest(): testing.", EagleEye::LOGLEVEL::EE_DEBUG);
-            EagleEye::Logger::CENTRAL_LOGGER().LogMessage(QString("LoggerTest::LoggerPerfomanceTest(): testing.").toStdString(), EagleEye::LOGLEVEL::EE_DEBUG);
-            EagleEye::Logger::CENTRAL_LOGGER().LogMessage(stdtestLog, EagleEye::LOGLEVEL::EE_DEBUG);
-        }
-        auto stop = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-        std::cout << i << duration.count() << " us" << std::endl;
+        EagleEye::Logger::CENTRAL_LOGGER().LogMessage(testLog.toStdString(), EagleEye::LOGLEVEL::EE_DEBUG);
+        EagleEye::Logger::CENTRAL_LOGGER().LogMessage("LoggerTest::LoggerPerfomanceTest(): testing.", EagleEye::LOGLEVEL::EE_DEBUG);
+        EagleEye::Logger::CENTRAL_LOGGER().LogMessage(QString("LoggerTest::LoggerPerfomanceTest(): testing.").toStdString(), EagleEye::LOGLEVEL::EE_DEBUG);
+        EagleEye::Logger::CENTRAL_LOGGER().LogMessage(stdtestLog, EagleEye::LOGLEVEL::EE_DEBUG);
     }
 }
 
